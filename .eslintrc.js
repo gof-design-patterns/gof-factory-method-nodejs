21ed24b9465600386be922b81b784494471b8263
Michael Silva <michaeldf.ti@gmail.com>
2022-03-19 16:41:28 -0300

    chore: add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: 'standard-with-typescript',
   parserOptions: {
-    project: './tsconfig.json'
+    project: './tsconfig.json',
+    warnOnUnsupportedTypeScriptVersion: false
   }
 }
